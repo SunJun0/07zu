c51684c8734ce21d61f6bba7da9df9da24bf5939
1825334857@qq.com <1825334857@qq.com>
2019-02-26 13:42:29 +0800

    dong

--- DAL/AchievementssDal.cs
+++ DAL/AchievementssDal.cs
@@ -12,7 +12,7 @@ namespace DAL
     {
         public List<Achievements> GetAchievements()//上月业绩排名
         {
-            string sql = "select ROW_NUMBER() over(order by  ASale DESC ) RowNum,Employees.EName,Employees.ESex,Achievements.ASale  from Employees join Achievements on Employees.EId =Achievements.AEId where  DATEPART(m,Achievements.AMonth)= MONTH(GETDATE())-1 ";
+            string sql = "select ROW_NUMBER() over(order by  ASale DESC ) RowNum,Employees.EName,Employees.ESex,Achievements.ASale  from Employees join Achievements on Employees.EId =Achievements.AEId where  DATEPART(m,Achievements.AMonth)= MONTH(GETDATE())-1  and Employees.EState =0";
             DataTable dt = DBHelper.GetDataTable(sql);
            List<Achievements> list =  JsonConvert.DeserializeObject<List<Achievements>>(JsonConvert.SerializeObject(dt));
             return list;
